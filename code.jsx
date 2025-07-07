03ea0274aad3b621230acb71a132f8ddf55c0980
NamiMIURA <735namiko@gmail.com>
2025-07-07 12:42:35 +0900

    code.jsxをMDNのコーディング規約に則り修正

---
+++ code.jsx
@@ -0,0 +1,12 @@
+function addNumbers(num1, num2) {
+  return n1 + n2;
+}
+
+function handleText(inputA, inputB) {
+  if (inputA === "test" && inputB !== "temp") {
+    return inputB;
+  }
+  return `${inputB} random text`;
+}
+
+let numOfCats = 0;
