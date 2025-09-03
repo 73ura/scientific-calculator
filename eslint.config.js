e6b667a1c8ed44d62d636959dd22fa4b05cc7f00
NamiMIURA <735namiko@gmail.com>
2025-09-03 10:53:42 +0900

    refactor: Next.js用の適切な設定に更新

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,22 @@
-export default [
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    ignores: ["eslint.config.js"],
-    files: ["**/*.{js,jsx}"], // ← JSXも含めるように！
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-    },
     rules: {
       "no-alert": "error",
       "id-length": ["error", { max: 12 }],
     },
   },
 ];
+
+export default eslintConfig;
